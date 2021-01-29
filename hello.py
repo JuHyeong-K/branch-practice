dd559c673ee4b6394686a919552ca1eaa1326afd
JuHyeong-K <wngud4680@gmail.com>
2021-01-29 11:24:54 +0900

    feat: Recreate print function

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
-for _ in range(5):
-    print('Hello Git!')
-
+def print_hello(): 
+    for i in range(1, 10):
+        print('Hello Git for {}th time(s)!'.format(i))
+if __name__ == '__main__':
+    print_hello()
